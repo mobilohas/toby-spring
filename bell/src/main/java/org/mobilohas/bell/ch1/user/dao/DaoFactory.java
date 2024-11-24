704635feac8be6971ecf353eea8bc6f54081af6d
pythonstrup <pythonstrup@gmail.com>
2024-11-24 22:22:05 +0900

    스프링 사용 시작

--- bell/src/main/java/org/mobilohas/bell/ch1/user/dao/DaoFactory.java
+++ bell/src/main/java/org/mobilohas/bell/ch1/user/dao/DaoFactory.java
@@ -1,19 +1,27 @@
 package org.mobilohas.bell.ch1.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
+  @Bean
   public UserDao userDao() {
     return new UserDao(connectionMaker());
   }
 
+  @Bean
   public AccountDao accountDao() {
     return new AccountDao(connectionMaker());
   }
 
+  @Bean
   public MessageDao messageDao() {
     return new MessageDao(connectionMaker());
   }
 
+  @Bean
   public ConnectionMaker connectionMaker() {
     return new DConnectionMaker();
   }
