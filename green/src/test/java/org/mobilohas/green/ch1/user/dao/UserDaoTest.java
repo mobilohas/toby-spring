e0460255ad666acb8a047601bd5398c51652c832
green <green@mobidoc.us>
2024-11-27 22:51:03 +0900

    feat: 1.8 XML 설정

--- green/src/test/java/org/mobilohas/green/ch1/user/dao/UserDaoTest.java
+++ green/src/test/java/org/mobilohas/green/ch1/user/dao/UserDaoTest.java
@@ -3,17 +3,18 @@ package org.mobilohas.green.ch1.user.dao;
 import org.mobilohas.green.ch1.user.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
 
 class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("green4");
+        user.setId("green5");
         user.setName("홍길동");
         user.setPassword("hong1234");
 
