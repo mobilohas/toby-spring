cb0c915e2b2e87b2f177d5f2d740fd6a5fa95008
pythonstrup <pythonstrup@gmail.com>
2024-12-11 22:06:52 +0900

    JDBC - 로컬 클래스 활용

--- bell/src/main/java/org/mobilohas/bell/ch3/user/dao/UserDao.java
+++ bell/src/main/java/org/mobilohas/bell/ch3/user/dao/UserDao.java
@@ -19,7 +19,19 @@ public class UserDao {
   }
 
   public void add(User user) throws SQLException {
-    StatementStrategy st = new AddStatement(user);
+    class AddStatement implements StatementStrategy {
+      @Override
+      public PreparedStatement makePreparedStatement(final Connection c) throws SQLException {
+        PreparedStatement ps = c.prepareStatement(
+            "insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+        return ps;
+      }
+    }
+
+    StatementStrategy st = new AddStatement();
     jdbcContextWithStatementStrategy(st);
   }
 
