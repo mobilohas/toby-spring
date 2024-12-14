36f15b8f2644e57ed20119428a18e90f1d4831ed
pythonstrup <pythonstrup@gmail.com>
2024-12-14 19:36:33 +0900

    JDBC - queryForObject

--- bell/src/main/java/org/mobilohas/bell/ch3/user/dao/UserDao.java
+++ bell/src/main/java/org/mobilohas/bell/ch3/user/dao/UserDao.java
@@ -1,12 +1,8 @@
 package org.mobilohas.bell.ch3.user.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.mobilohas.bell.ch3.user.domain.User;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserDao {
@@ -30,40 +26,19 @@ public class UserDao {
     jdbcTemplate.update("delete from users");
   }
 
-//  public User get(String id) throws SQLException {
-//    Connection c = dataSource.getConnection();
-//
-//    PreparedStatement ps = c.prepareStatement(
-//        "select * from users where id = ?");
-//    ps.setString(1, id);
-//
-//    ResultSet rs = ps.executeQuery();
-//
-//    User user = null;
-//    if (rs.next()) {
-//      user = new User();
-//      user.setId(rs.getString("id"));
-//      user.setName(rs.getString("name"));
-//      user.setPassword(rs.getString("password"));
-//    }
-//
-//    rs.close();
-//    ps.close();
-//    c.close();
-//
-//    if (user == null) {
-//      throw new EmptyResultDataAccessException(1);
-//    }
-//
-//    return user;
-//  }
+  public User get(String id) throws SQLException {
+    return jdbcTemplate.queryForObject("select * from users where id = ?",
+        new Object[]{id}, // SQL에 바인딩할 파라미터 값, 가변인자 대신 배열을 사용한다.
+        (rs, i) -> { // ResultSet 한 로우의 결과를 오브젝트에 매핑해주는 RowMapper 콜백
+          User user = new User();
+          user.setId(rs.getString("id"));
+          user.setName(rs.getString("name"));
+          user.setPassword(rs.getString("password"));
+          return user;
+        });
+  }
 
   public int getCount() throws SQLException {
-    return jdbcTemplate.query(
-        con -> con.prepareStatement("select count(*) from users"),
-        rs -> {
-          rs.next();
-          return rs.getInt(1);
-        });
+    return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
   }
 }
