327eb1f87a39179a11306ec5e069cb9a2dc726a4
sudong <sudong@mobidoc.us>
2024-12-15 15:36:24 +0900

    [sudong] 3장 정리

--- sudong/src/main/java/org/mobilohas/sudong/ch1/user/dao/UserDao.java
+++ sudong/src/main/java/org/mobilohas/sudong/ch1/user/dao/UserDao.java
@@ -5,19 +5,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.mobilohas.sudong.ch1.user.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import javax.sql.DataSource;
 
 public class UserDao {
-  private ConnectionMaker connectionMaker;
 
-  public UserDao(ConnectionMaker connectionMaker) {
-    this.connectionMaker = connectionMaker;
+  private DataSource dataSource;
+
+  public UserDao() {}
+
+  public UserDao(final DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   public void add(User user) throws ClassNotFoundException, SQLException {
-    Connection c = connectionMaker.getConnection();
+    Connection c = dataSource.getConnection();
 
     PreparedStatement ps = c.prepareStatement(
-        "insert into users(id, name, password) values(?,?,?)");
+            "insert into users(id, name, password) values(?,?,?)");
     ps.setString(1, user.getId());
     ps.setString(2, user.getName());
     ps.setString(3, user.getPassword());
@@ -29,23 +35,59 @@ public class UserDao {
   }
 
   public User get(String id) throws ClassNotFoundException, SQLException {
-    Connection c = connectionMaker.getConnection();
+    Connection c = dataSource.getConnection();
 
     PreparedStatement ps = c.prepareStatement(
-        "select * from users where id = ?");
+            "select * from users where id = ?");
     ps.setString(1, id);
 
     ResultSet rs = ps.executeQuery();
-    rs.next();
-    User user = new User();
-    user.setId(rs.getString("id"));
-    user.setName(rs.getString("name"));
-    user.setPassword(rs.getString("password"));
+
+    User user = null;
+    if (rs.next()) {
+      user = new User();
+      user.setId(rs.getString("id"));
+      user.setName(rs.getString("name"));
+      user.setPassword(rs.getString("password"));
+    }
 
     rs.close();
     ps.close();
     c.close();
 
+    if (user == null) {
+      throw new EmptyResultDataAccessException(1);
+    }
+
     return user;
+  }
+
+  public int getCount() throws ClassNotFoundException, SQLException {
+    Connection c = dataSource.getConnection();
+
+    PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+    ResultSet rs = ps.executeQuery();
+    rs.next();
+    int count = rs.getInt(1);
+
+    rs.close();
+    ps.close();
+    c.close();
+    return count;
+  }
+
+  public void deleteAll() throws ClassNotFoundException, SQLException {
+    Connection c = dataSource.getConnection();
+    PreparedStatement ps = c.prepareStatement("delete from users");
+    ps.executeUpdate();
+
+    ps.close();
+    c.close();
+  }
+
+  // XML 설정을 위한 Setter
+  public void setDataSource(final DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 }
